5e87f8b87e1b4903fafbe4619f5b593a65d41277
SHASHANKA1996 <shashankarao1996@gmail.com>
2017-07-16 01:46:59 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+var n1=prompt("enter 1st number");
+var n2=prompt("enter 2nd number");
+var n3=prompt("enter 3rd number");
+
+
+if(n1>n2)
+ {
+	  if(n1>n3)
+	    {
+	document.write(n1);
+	    }
+else
+	{
+	 document.write(n3);
+	}
+ }
+else if(n2>n3)
+	{
+	document.write(n2);
+	}
+else 
+	{
+	document.write(n3);
+	}
